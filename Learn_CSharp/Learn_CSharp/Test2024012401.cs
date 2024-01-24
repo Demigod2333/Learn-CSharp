4794bbce4795b0f81a590a2eed85bd0398a6642a
Demigod <546556072@qq.com>
2024-01-24 10:07:40 +0800

    20240123
    Main方法和顶级语句

---
+++ Learn_CSharp/Learn_CSharp/Test2024012401.cs
@@ -0,0 +1,14 @@
+﻿/*using System.Text;
+
+StringBuilder builder = new();
+builder.AppendLine("Hello");
+builder.AppendLine("World!");
+
+Console.WriteLine(builder.ToString());*/
+
+class TestClass { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
